889c9ee06bcb6e76991786f5e7e3fe86b9007f4c
Celso Ferreira <capcelsoricardo@gmail.com>
2019-09-05 09:08:32 -0300

    4. Encontro

--- src/test/java/curso/treinamento/runners/RunnerTest.java
+++ src/test/java/curso/treinamento/runners/RunnerTest.java
@@ -10,7 +10,7 @@ import cucumber.api.junit.Cucumber;
 @CucumberOptions(
 		features = {"src/test/resources/features/"},
 		glue	 = {"curso.treinamento.steps"},
-		snippets = SnippetType.CAMELCASE	
+		snippets = SnippetType.UNDERSCORE
 		)
 
 
--- src/test/java/curso/treinamento/steps/LoginSteps.java
+++ src/test/java/curso/treinamento/steps/LoginSteps.java
@@ -1,5 +1,45 @@
 package curso.treinamento.steps;
 
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Então;
+import cucumber.api.java.pt.Quando;
+
 public class LoginSteps {
+	
+	
+	public WebDriver driver;
+	
+	@Dado("que eu deseje logar no sistema")
+	public void que_eu_deseje_logar_no_sistema() {
+		
+		System.setProperty("webdriver.chrome.driver", "src/test/resources/mac/chromedriver");
+		driver = new ChromeDriver();
+		
+		driver.get("https://www.phptravels.net/admin");
+		
+		
+	}
+
+	@Quando("faço login com o usuário {string} e senha {string}")
+	public void faço_login_com_o_usuário_e_senha(String user, String pass) {
+		
+		driver.findElement(By.name("email")).sendKeys(user);
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys(pass);		
+		driver.findElement(By.xpath("//span[text()='Login']")).click();
+		
+	}
+
+	@Então("sou autenticado com sucesso")
+	public void sou_autenticado_com_sucesso() throws InterruptedException {
+		
+		Thread.sleep(5000);
+		Assert.assertTrue(driver.findElement(By.xpath("//a[@href='https://www.phptravels.net/admin/logout']")).isDisplayed());
+	}
 
 }
